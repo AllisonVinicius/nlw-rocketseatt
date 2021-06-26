db1efb186a9e6ef6c54fe7fc43a0ddbd97ad0ea9
allison <allisonvinicus63@gmail.com>
2021-06-26 00:04:27 -0400

    component button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,8 @@
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes } from "react"; //atributos do botao pode receber
+import "../styles/button.scss";
 
-type BuuttonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type BuuttonProps = ButtonHTMLAttributes<HTMLButtonElement>; //tipagem global
 
 export function Button(props: BuuttonProps) {
-  return <button className="button" {...props} />;
+  return <button className="button" {...props} />; //spread operator
 }
