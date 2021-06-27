7ffc23702c1f1a2aeb2f8f4c71d55a19d6d1a0d6
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-27 12:27:11 -0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 // import { Home } from "./pages/Home";
 
-
 import { BrowserRouter, Route } from "react-router-dom";
-import { auth, firebase } from "./services/firebase";
+
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
-import {AuthContextProvider} from './contexts/AuthContext';
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
-return (
-  <BrowserRouter>
-    <AuthContextProvider>  
-      <Route path="/" exact component={Home} />
-      <Route path="/roooms/new" component={NewRoom} />
-    </AuthContextProvider>
-  </BrowserRouter>
-);
-
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/roooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
 export default App;
