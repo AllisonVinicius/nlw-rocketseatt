a56545a68fad3f0787349960aec876a17d44f731
allison <allisonvinicus63@gmail.com>
2021-06-27 00:37:01 -0400

    useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 // import { Home } from "./pages/Home";
+import { createContext, useState } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-
+import { auth, firebase } from "./services/firebase";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
+export const AuthContext = createContext({} as any);
+
 function App() {
+  const [user, setUser] = useState();
+
+  function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider(); //autenticacao com google
+
+    auth.signInWithPopup(provider).then((result) => {
+      if (result.user) {
+        const { displayName, photoURL, uid } = result.user;
+        if (!displayName || !photoURL) {
+          throw new Error("missing informatioc google");
+        }
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+        });
+      }
+    });
+  }
+
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/roooms/new" component={NewRoom} />
+      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+        <Route path="/" exact component={Home} />
+        <Route path="/roooms/new" component={NewRoom} />
+      </AuthContext.Provider>
     </BrowserRouter>
   );
 }
