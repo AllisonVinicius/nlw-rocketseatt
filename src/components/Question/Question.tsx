b1853a10bec66f117c55df8b308b1540d18e98d9
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-28 00:48:17 -0400

    pag questoes

---
+++ src/components/Question/Question.tsx
@@ -0,0 +1,44 @@
+import { ReactNode } from "react";
+import cx from "classnames";
+
+import "./styles.scss";
+
+type QuestionProps = {
+  //props contendo info necessárias das questões
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
+};
+
+export function Question({
+  content,
+  author,
+  isAnswered = false,
+  isHighlighted = false,
+  children,
+}: QuestionProps) {
+  //desestruturando obje
+  return (
+    <div
+      className={cx(
+        "question",
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered }
+      )}
+    >
+      <p>{content}</p>
+      <footer>
+        <div className="user-info">
+          <img src={author.avatar} alt={author.name} />
+          <span>{author.name}</span>
+        </div>
+        <div>{children}</div>
+      </footer>
+    </div>
+  );
+}
